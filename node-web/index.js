e746cf2f91a46af481f7b4b0eca05aa78fa74dad
shan shaji <shanshaji224@gmail.com>
2020-12-27 20:17:25 +0530

    Added module

--- node-web/index.js
+++ node-web/index.js
@@ -2,20 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const { type } = require("os");
 const url = require("url");
+const replaceTemplate = require(`./modules/replaceTemplate`)
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace("<%PRODUCTNAME%>", product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const tempOverView = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
